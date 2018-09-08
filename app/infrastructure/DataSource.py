1c82f1c96cd46635a36a3c3845cb99af74304cda
Naoki Miyoshi <trombone344@gmail.com>
2018-09-08 13:15:22 +0900

    fix DBデータを取得するクラスを修正する

--- app/infrastructure/DataSource.py
+++ app/infrastructure/DataSource.py
@@ -22,27 +22,12 @@ class DataSource():
             'database' : db_name
         }
 
-    def get_row( self, table_name, where_array ):
-        db = mysql.connector.connect(**self.config)
+    def get_db_data( self, query, parameter ):
+        self.db = mysql.connector.connect(**self.config)
         cursor = db.cursor()
-        #val_array = [ table_name ]
-        val_array = []
-        sql = "select * from " + table_name
-        for key,value in where_array.items():
-            sql = sql + " WHERE "
-            sql = sql + key + "=" + value
-        #    val_array.append( key )
-        #    val_array.append( value )
-
-        print ( sql )
-        #cursor.execute(sql , tuple(val_array) )
-        cursor.execute(sql)
-        row = cursor.fetchall()
-        print( *row,sep='\n' )
+        cursor.execute(query, parameter)
+        self.data = cursor.fetchall()
         cursor.close()
         db.close()
 
-
-datasource = DataSource()
-datasource.get_row('folder', { 'folder_ID' : 1})
 
