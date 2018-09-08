90f99250d86cf4e48eaf3e216b012506aef38d69
Naoki Miyoshi <trombone344@gmail.com>
2018-09-08 17:43:12 +0900

    add 辞書形式で取得できるメソッドを追加

--- lib/model/folder.py
+++ lib/model/folder.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
-import os
 from enum import Enum
+import json
 import datetime
-from test_mod import *
 
 
 '''
 Value Object : ID
 '''
+class FolderID:
+  def __init__( self,folder_id ):
+    self.value = folder_id
+
+  def __check_exist_user(self):
+    return True
+
+'''
+Value Object : AuthorID
+'''
 class FolderAuthorID:
   def __init__( self,user_id ):
-    self.value = 1
+    self.value = user_id
 
   def __check_exist_user(self):
     return True
@@ -43,7 +52,7 @@ Value Object : LastUpdateDate
 class FolderLastUpdateDate:
 
   def __init__( self,date ):
-    self.value = date
+      self.value = date
 
 
 '''
@@ -52,13 +61,14 @@ Value Object : RegisterDate
 class FolderRegisterDate:
 
   def __init__( self,date ):
-    self.value = date
+      self.value = date
 
 
 '''
 Value Object : ReleaseStatus
 '''
 class FolderReleaseStatus(Enum):
+  NONE = 0
   OPEN = 1
   CLOSE = 2
 
@@ -67,6 +77,7 @@ class FolderReleaseStatus(Enum):
 Value Object : ShareRange
 '''
 class FolderShareRange(Enum):
+  NONE = 0
   PRIVATE = 1
   PUBLICK = 2
   PASSFRASE = 3
@@ -89,37 +100,39 @@ class FolderThumbnailUrl:
 
 class Folder():
 
-  def __init__( self,
-               author_id : FolderAuthorID,
-               name : FolderName,
-               description : FolderDescription,
-               last_update_date : FolderLastUpdateDate,
-               register_date : FolderRegisterDate,
-               release_status : FolderReleaseStatus,
-               share_range : FolderShareRange,
-               share_url : FolderShareUrl,
-               thumbnail_url : FolderThumbnailUrl):
+    def __init__( self,
+        folder_id : FolderID,
+        author_id : FolderAuthorID,
+        name : FolderName,
+        description : FolderDescription,
+        last_update_date : FolderLastUpdateDate,
+        register_date : FolderRegisterDate,
+        release_status : FolderReleaseStatus,
+        share_range : FolderShareRange,
+        share_url : FolderShareUrl,
+        thumbnail_url : FolderThumbnailUrl):
 
-    self.author = FolderAuthorID( author_id )
-    self.name = FolderName( name )
-    self.description = FolderDescription( description )
-    self.register_date = FolderRegisterDate( register_date )
-    self.last_update_date = FolderLastUpdateDate( last_update_date )
-    self.release_status = FolderReleaseStatus( release_status )
-    self.share_range = FolderShareRange( share_range )
-    self.share_url = FolderShareUrl( share_url )
-    self.thumbnail_url = FolderThumbnailUrl( thumbnail_url )
+        self.id = FolderID( folder_id )
+        self.author_id = FolderAuthorID( author_id )
+        self.name = FolderName( name )
+        self.description = FolderDescription( description )
+        self.register_date = FolderRegisterDate( register_date )
+        self.last_update_date = FolderLastUpdateDate( last_update_date )
+        self.release_status = FolderReleaseStatus( release_status )
+        self.share_range = FolderShareRange( share_range )
+        self.share_url = FolderShareUrl( share_url )
+        self.thumbnail_url = FolderThumbnailUrl( thumbnail_url )
 
-
-# インスタンスの作成
-folder_obj = Folder(1,'taro', '太郎のフォルダです', datetime.datetime.today(), datetime.datetime.today(), 1, 2,'https://share.url', 'https://thumbnail.url')
-print ( folder_obj.author.value )
-print ( folder_obj.name.value )
-print ( folder_obj.description.value )
-print ( folder_obj.last_update_date.value )
-print ( folder_obj.register_date.value )
-print ( folder_obj.release_status.value )
-print ( folder_obj.share_range.name )
-print ( folder_obj.share_url.value )
-print ( folder_obj.thumbnail_url.value )
-
+    def to_dict(self) :
+        return {
+            "folder_id" : self.id.value,
+            "author_id" : self.author_id.value,
+            "name" : self.name.value,
+            "description" : self.description.value,
+            "register_date" : self.register_date.value.isoformat(),
+            "last_update_date" : self.last_update_date.value.isoformat(),
+            "release_status" : self.release_status.value,
+            "share_range" : self.share_range.value,
+            "share_url" : self.share_url.value,
+            "thumbnail_url" : self.thumbnail_url.value,
+        }
