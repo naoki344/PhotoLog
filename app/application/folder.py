f688c2cd4374d6c691448fc26520ea235685f224
Naoki Miyoshi <trombone344@gmail.com>
2018-09-08 17:46:47 +0900

    add フォルダの一覧取得機能を追加

--- app/application/folder.py
+++ app/application/folder.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 
-class GetAllUserFolder():
+import pprint
+import json
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+from infrastructure.folder import FolderDataSource
+from model.folder import FolderAuthorID
 
-    def __init__( self, author_id : AuthorID ):
+class GetUserAllFolder():
 
+    def __init__( self, folder_author_id : FolderAuthorID ):
+        fd = FolderDataSource()
+        folders = fd.get_all_folder(folder_author_id)
+        folder_dic_array = []
+        for folder_obj in folders :
+            folder_dic_array.append( folder_obj.to_dict() )
+
+        #pprint.pprint ( folder_dic_array ,indent=4,depth=None)
+        json_txt = json.dumps( folder_dic_array ,indent=4)
+        print ( json_txt )
+
+GetUserAllFolder(1)
