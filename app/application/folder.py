258137ab28a76313e5c9a2d6687137ecbf968376
Naoki Miyoshi <trombone344@gmail.com>
2018-09-13 00:09:01 +0900

    fix applicationとuserinterfaceを分離

--- app/application/folder.py
+++ app/application/folder.py
@@ -1,23 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import pprint
-import json
 import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 from infrastructure.folder import FolderDataSource
 from model.folder import FolderAuthorID
 
-class GetUserAllFolder():
+class GetAllFolderService():
 
     def __init__( self, folder_author_id : FolderAuthorID ):
-        fd = FolderDataSource()
-        folders = fd.get_all_folder(folder_author_id)
-        folder_dic_array = []
-        for folder_obj in folders :
-            folder_dic_array.append( folder_obj.to_dict() )
+        folderdata = FolderDataSource()
+        self.folders = folderdata.get_all_folder(folder_author_id)
 
-        #pprint.pprint ( folder_dic_array ,indent=4,depth=None)
-        json_txt = json.dumps( folder_dic_array ,indent=4)
-        print ( json_txt )
-
-GetUserAllFolder(1)
---
+++ app/userinterface/folder.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import pprint
+import json
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+from application.folder import GetAllFolderService
+from model.folder import FolderAuthorID
+
+class GetUserAllFolder():
+
+    def __init__( self, folder_author_id : FolderAuthorID ):
+        folder = GetAllFolderService(folder_author_id)
+        self.all_folder = []
+        for folder_obj in folder.folders :
+            self.all_folder.append( folder_obj.to_dict() )
+        json_txt = json.dumps( self.all_folder ,indent=4)
+
+        print ( json_txt )
+
+
+
+GetUserAllFolder(1)
