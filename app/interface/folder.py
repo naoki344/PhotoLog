56cac7e604b4929ac8c2c4c0a374b9b3bb44b2cd
Your Name <you@example.com>
2018-09-16 08:08:44 +0000

    fix フォルダ名の変更すã

--- app/interface/folder.py
+++ app/interface/folder.py
@@ -3,17 +3,17 @@
 import pprint
 import json
 import sys,os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
-from application.folder import GetAllFolderService
-from model.folder import FolderAuthorID
+from app.application.folder import GetAllFolderService
+from lib.model.folder import FolderAuthorID
 
 class GetUserAllFolder():
 
     def __init__( self, folder_author_id : FolderAuthorID ):
-        folder = GetAllFolderService(folder_author_id)
+        self.folder = GetAllFolderService(folder_author_id)
+       	
+    def get_json(self,):
         self.all_folder = []
-        for folder_obj in folder.folders :
+        for folder_obj in self.folder.folders :
             self.all_folder.append( folder_obj.to_dict() )
         json_txt = json.dumps( self.all_folder ,indent=4)
-
         return json_txt
