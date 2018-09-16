b41cfe9407d5896aa0c9097d667e9c6fe14a76cd
Your Name <you@example.com>
2018-09-16 16:08:17 +0000

    fix flask use

--- api_interface/app_router.py
+++ api_interface/app_router.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-
-def application(env, start_response):
-    doc_root = '/photo_log'
-    path = env['PATH_INFO']
-    if path == doc_root + '/':
-        start_response('200 OK', [('Content-type', 'text/plain')])
-        return [b'Hello World']
-    elif path == doc_root + '/foo':
-        start_response('200 OK', [('Content-type', 'text/plain')])
-        return [b'foo']
-    else:
-        start_response('404 Not Found', [('Content-type', 'text/plain')])
-        return [b'404 Not Found']
+#
+import sys
+from flask import Flask
+sys.path.append('/var/www/PhotoLog/api_interface/')
+application = Flask(__name__)
+@application.route('/photo_log')
+def index():
+    return 'HelloWorld'
