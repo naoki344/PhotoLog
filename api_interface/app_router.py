19599b9cb169a8e94777b55b8bb8cfe57edfac07
Your Name <you@example.com>
2018-09-17 07:40:00 +0000

    add folderを返す機能を追加

--- api_interface/app_router.py
+++ api_interface/app_router.py
@@ -6,8 +6,15 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 from app.interface.folder import GetUserAllFolder
 
 application = Flask(__name__)
-@application.route('/photo_log')
+
+@application.route('/photo_log/')
 def index():
+    data = GetUserAllFolder(1)
+    json = data.get_json()
+    return json.encode("UTF-8")
+
+@application.route('/photo_log/folder/')
+def folder():
     data = GetUserAllFolder(1)
     json = data.get_json()
     return json.encode("UTF-8")
