fc2c1483924603b71c37f66a46033e09596733af
Naoki Miyoshi <trombone344@gmail.com>
2018-09-25 21:40:54 +0900

    fix 辞書型への変換機能の修正

--- lib/model/folder/folder_list.py
+++ lib/model/folder/folder_list.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 from enum import Enum
-import json
 import datetime
 from lib.model.folder.folder import Folder
 
 class FolderList():
 
     def __init__( self, folder_obj_list ):
-        self.uset_folder_list = folder_obj_list
+        self.folder_list = folder_obj_list
 
     def add_folder( self, folder : Folder ):
-        self.uset_folder_list.append( folder )
+        self.folder_list.append( folder )
 
     def to_dict(self):
-        self.folder_list = []
-        for folder_obj in self.uset_folder_list :
-            self.folder_list.append( folder_obj.to_dict() )
+        folder_dict_list = []
+        for folder_obj in self.folder_list :
+            folder_dict_list.append( folder_obj.to_dict() )
 
-        self.json_txt = json.dumps( self.folder_list ,indent=4)
-        return self.json_txt
+        return folder_dict_list
