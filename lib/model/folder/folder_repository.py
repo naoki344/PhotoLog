be71c2fe4a1faab2fc8b8f9b3fac918e36b193c6
Naoki Miyoshi <trombone344@gmail.com>
2018-09-30 00:55:31 +0900

    fix メソッド名の追加

--- lib/model/folder/folder_repository.py
+++ lib/model/folder/folder_repository.py
@@ -11,21 +11,21 @@ from lib.model.folder.folder import FolderID
 class FolderRepository(metaclass=ABCMeta):
 
     @abstractmethod
-    def get_all_folder(self, folder_author_id : FolderAuthorID ):
+    def find_user_all(self, folder_author_id : FolderAuthorID ):
         pass
 
     @abstractmethod
-    def find_folder(self, folder_id : FolderID ):
+    def find(self, folder_id : FolderID ):
         pass
 
     @abstractmethod
-    def register_folder(self, folder : Folder ):
+    def register(self, folder : Folder ):
         pass
 
     @abstractmethod
-    def update_folder(self, folder : Folder ):
+    def update(self, folder : Folder ):
         pass
 
     @abstractmethod
-    def delete_folder(self, folder_id : FolderID ):
+    def delete(self, folder_id : FolderID ):
         pass
