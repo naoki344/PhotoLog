ec44ff1aef4b5a821cf3d65bcc79bbda456528d8
miyoshi_naoki <miyoshi_naoki@aratana.jp>
2018-10-17 23:39:02 +0900

    Revert "linter"
    This reverts commit a40d93a2924e54e820911bff57e009c227e99c34.

--- vuejs_pwa/.eslintrc.js
+++ vuejs_pwa/.eslintrc.js
@@ -10,10 +10,7 @@ module.exports = {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: [
-      'standard',
-      'plugin:prettier/recommended'
-  ],
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
@@ -26,12 +23,5 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-    'prettier/prettier': [
-    'error',
-      {
-        'singleQuote': true,
-        'trailingComma': 'es5'
-      }
-  ]
   }
 }
