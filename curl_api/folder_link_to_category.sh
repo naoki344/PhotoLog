4f92bb4411ac802d3bed369faad452fc14f64e69
miyoshi_naoki <miyoshi_naoki@aratana.jp>
2018-12-04 10:32:48 +0900

    add 実行するshファイルを追加

---
+++ curl_api/folder_link_to_category.sh
@@ -0,0 +1,115 @@
+function link_f() {
+	CATEGORY_ID=${1}
+	FOLDER_ID=${2}
+	curl -X POST http://view.photolog.online/trombone344%40gmail.com/category/$CATEGORY_ID/folder/$FOLDER_ID
+	sleep 1
+}
+#[防大] common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a
+#link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-535c87a0-b515-443d-b7d8-1c857346fec8';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-73d79bda-1581-40f0-a9e9-a1281f80978b';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-320b8a8b-024e-46ce-b916-71bfa503e3a3';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-4a59e3e5-0bf8-4b0d-95b2-a36f451a8e60';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-0580d0f3-fe11-49bf-88a3-41eb308f30d0';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-854e6cd2-fe24-433b-99fd-92102a8b17b1';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-28d21612-b8f7-4778-aa34-a8f0b7f57594';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-a1af05bc-5c9a-44f8-90e1-c3fc3d0b5b85';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-f4f66110-30ef-41c0-906c-df21e3bde4b1';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-85820832-ef23-4c24-bdb1-807dd526a9d2';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-37b99ae1-57d7-4b18-9752-c911c0e6eff2';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-fc8bb6df-d0cd-47e0-b48e-479c4233e167';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-d3a5747b-d3ad-40d8-bd64-c19df19e2e15';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-f5a10322-2a06-4ff8-99bc-69a68dc9c93d';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-468d2e55-848c-466c-a125-0c3c9335e87b';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-01b6bad6-e685-4682-865e-0d4a223df304';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-f55ba79a-e099-479a-8b9f-560bf9fdb675';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-1a0a43e8-bd89-48b6-a357-ef602044ac04';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-941a3337-0405-44bd-83d4-b49c7872d645';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-0230318e-fc21-4606-8253-9efb6c08acd7';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-754e71fe-18f8-44f3-95f3-e7a5064ea4f4';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-a8d4fcc9-ad1d-4f38-a864-9e03557914de';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-7a4b2c8e-0150-470b-8f92-15593dc31db9';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-ddd842cc-39a5-413e-ad77-6f98a5fb93ef';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-51844a81-637f-4cf0-85ba-add8a41cf2c5';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-d3942bb2-b632-4980-b89f-4e8f3dcf4d72';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-de1b9b19-0110-4d19-852c-d015bb52249b';
+link_f 'common_category-11b24aea-ae04-4afb-a4d9-d03120d2716a' 'folder-4d914979-f366-4138-a3ae-089ec3629ecf';
+
+#[家族写真] common_category-b9ad8698-413b-432a-b438-d9e1a78f255e
+link_f 'common_category-b9ad8698-413b-432a-b438-d9e1a78f255e' 'folder-ddbe0e5b-59a7-468f-bf1e-73534f073f14'
+link_f 'common_category-b9ad8698-413b-432a-b438-d9e1a78f255e' 'folder-c4ed811b-5dd0-404b-86e2-714ac61b16b1'
+
+
+#[Art] common_category-08819f66-77f6-4183-811a-456847c01a87
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-37b99ae1-57d7-4b18-9752-c911c0e6eff2'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-fc8bb6df-d0cd-47e0-b48e-479c4233e167'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-d3a5747b-d3ad-40d8-bd64-c19df19e2e15'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-468d2e55-848c-466c-a125-0c3c9335e87b'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-de1b9b19-0110-4d19-852c-d015bb52249b'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-db9ef5e4-0ea7-4b5c-a296-f8ce8c473173'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-9485bb2e-16d8-47fd-8329-49ae7ec9cdd1'
+link_f 'common_category-08819f66-77f6-4183-811a-456847c01a87' 'folder-a26d5260-a0b0-46be-bde4-67e3b330785f'
+
+#[宮崎] common_category-4d4a400a-f76a-470b-8389-c6b361a492e0
+link_f 'common_category-4d4a400a-f76a-470b-8389-c6b361a492e0' 'folder-2a720623-d76d-48cd-88cd-07840c573165'
+link_f 'common_category-4d4a400a-f76a-470b-8389-c6b361a492e0' 'folder-9bd8f252-4f5f-41aa-a503-7f20ea6afcb5'
+link_f 'common_category-4d4a400a-f76a-470b-8389-c6b361a492e0' 'folder-2a047a23-a9e3-434e-8204-e9f303f82ddd'
+link_f 'common_category-4d4a400a-f76a-470b-8389-c6b361a492e0' 'folder-39440ea9-a11b-4e0a-9c56-296b210a904d'
+link_f 'common_category-4d4a400a-f76a-470b-8389-c6b361a492e0' 'folder-b63f442f-891d-42d2-b844-7ba65c97b0ab'
+
+#[久美子] common_category-4aed8946-595d-4866-99f8-5d9cbf87311c
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-5aaa8cb6-d2f8-4955-bf3b-a3a8f873e382'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-b4147f1a-70a6-4e2e-8248-6ddd9ab5e2bf'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-68b80ad6-85e1-4eb4-aeb1-aca31913e120'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-7715ae79-24ad-479f-9904-8ca18890b89a'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-06a4ce00-c2b9-4940-ad84-2e509d2573d8'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-ff5aafcb-3a98-47b3-9f5b-6dd37fcac6ed'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-eea4ced4-5b24-46dc-92fa-627700eb4ed8'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-f0c56bef-dd7e-4be1-aed0-91b857a95cef'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-7011d8c2-8b52-42f1-a2cb-67963e6be197'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-3a6c3761-76ba-4a71-9b33-6b60ecd4f72e'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-a2632c66-4be0-42ae-94c7-e869e6d2a969'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-9485bb2e-16d8-47fd-8329-49ae7ec9cdd1'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-a26d5260-a0b0-46be-bde4-67e3b330785f'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-81691c28-fdcc-49a3-b998-d5f5339b8e1c'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-023d18db-075b-4875-83d0-780bd575b454'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-5385f001-f6e6-4177-a07c-06f0eaf759ee'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-b157ab06-574a-411d-90d6-e4701d5df784'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-fe6959ab-4378-4e79-93b4-a0b79618905d'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-fb4b2a65-f5a0-4e45-849a-00389fafcbbc'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-062406aa-9a52-4fce-9a07-7ee23446d854'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-d335e4db-9aa6-47d2-be7d-a28ace9678b8'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-dbb4abcc-4b4c-4d33-9e98-62574fea50ba'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-2dbd5e25-853a-41bc-b9fe-38cfcba2ceb6'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-d8c7e6e2-2b93-4e34-b54e-9c88a3d83863'
+link_f 'common_category-4aed8946-595d-4866-99f8-5d9cbf87311c' 'folder-32c53596-a9ab-4e32-aff6-9e4a6258245d'
+
+
+#[24Crew] common_category-83b07ab6-975d-4a57-8787-55a26282a920
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-7fabb9cb-4f7f-45e7-aecd-d8d6faa017f0'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-b29c5481-172a-48bb-9b7d-9eb29159c9bd'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-764d5a6d-1e17-43d8-9037-01ed026593dd'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-1728d934-c586-4ae2-9ae3-8f80f1c98acc'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-39d22a42-7fd9-490b-9390-62d7658906fc'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-26f17520-f03b-4742-8b25-12845c103aa9'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-868a0c12-d436-47eb-aa34-6e461027adb7'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-1f035fdb-16a5-4c1a-9ddd-caa5c6998323'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-5a3592bc-716f-46ae-a9ea-cdddcdab17ff'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-8430fd53-6f44-4442-a737-d168bb677e9a'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-f4844cce-3bbf-4998-a213-444084a573ba'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-2e6f4358-9765-4e47-8379-77ce55a2e073'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-6089581c-7fe3-4e46-833a-4922010d914e'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-00bf8a3e-707e-428e-a1d4-cb658188a84b'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-6d5a7458-d374-45f9-9515-ac1bce8ba550'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-8823c018-31fb-4a50-8f7b-5704418d03bd'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-55d90450-17f4-4c14-b60d-10b8dd64343b'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-b6b80c9a-b196-4636-9fe7-4b8d69afced7'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-eb7c3a67-a126-47a6-906f-7f91e287d7cd'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-44155bfc-da35-487d-800f-a5389a881ca2'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-90d499c1-9e6d-4a68-a468-d7e19c85f0f4'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-9a77edc2-e5dc-4fb2-a6b5-997c25757735'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-576e4607-6001-4aa1-921d-956b17ca75c4'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-65f907e1-8341-4d05-b5b7-63839ef42508'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-afc4080e-e4a0-4688-ac39-bfb927d3650a'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-d92282bd-d89c-46e1-bbfa-60a8da20fa9d'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-97fc9b93-ae52-45d6-a054-fd5674d9b5f5'
+link_f 'common_category-83b07ab6-975d-4a57-8787-55a26282a920' 'folder-d119ebb3-0063-455b-93f3-ae2b76969794'
